e837d38662861716b90506c27fd534e7507e1674
Em3L <martink_lopez@yahoo.com>
2023-07-22 15:37:15 +1000

    added readme generating code to index and first line of code to generateMarkdown

--- potential-enigma-main-clone/Develop/index.js
+++ potential-enigma-main-clone/Develop/index.js
@@ -1,13 +1,117 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'projectTitle',
+    message: 'Enter your project title:'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information:'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter contribution guidelines:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions:'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GNU GPL v3', 'ISC', 'Unlicense', 'Other']
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Enter your GitHub username:'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:'
+  }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to generate the README content
+function generateReadme(data) {
+  return `
+# ${data.projectTitle}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is covered under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions or concerns regarding this project, please contact ${
+    data.githubUsername
+  } at ${data.email}.
+`;
+}
+
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README.md generated successfully!');
+    }
+  });
+}
+
+// Function to initialize app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const readmeContent = generateReadme(answers);
+      writeToFile('README.md', readmeContent);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 // Function call to initialize app
 init();
--- potential-enigma-main-clone/Develop/utils/generateMarkdown.js
+++ potential-enigma-main-clone/Develop/utils/generateMarkdown.js
@@ -1,6 +1,25 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on the license passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'GNU GPL v3':
+      return '[![License: GNU GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'ISC':
+      return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+    case 'Unlicense':
+      return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+    case 'Other':
+      // Customize this link to the license of your choice or remove this case if not needed.
+      return '[![License: Custom License](https://img.shields.io/badge/License-Custom%20License-red.svg)](https://example.com/custom-license)';
+    default:
+      return '';
+  }
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
