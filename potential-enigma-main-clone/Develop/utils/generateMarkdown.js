d4b51043042ea1c9813d7d6eee0cde7f9a49cd1b
Em3L <martink_lopez@yahoo.com>
2023-07-22 15:38:45 +1000

    added the Function that returns the license link based on the license passed in

--- potential-enigma-main-clone/Develop/utils/generateMarkdown.js
+++ potential-enigma-main-clone/Develop/utils/generateMarkdown.js
@@ -20,10 +20,27 @@ function renderLicenseBadge(license) {
   }
 }
 
-
-// TODO: Create a function that returns the license link
+// Function that returns the license link based on the license passed in
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GNU GPL v3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'ISC':
+      return 'https://opensource.org/licenses/ISC';
+    case 'Unlicense':
+      return 'http://unlicense.org/';
+    case 'Other':
+      // Customize this link to the license of your choice or remove this case if not needed.
+      return 'https://example.com/custom-license';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
