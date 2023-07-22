7722d1e48e6a03163c1f261f56c076a5c749bf18
Em3L <martink_lopez@yahoo.com>
2023-07-22 15:46:51 +1000

    added the Function that returns the license section of README based on the license passed in

--- potential-enigma-main-clone/Develop/utils/generateMarkdown.js
+++ potential-enigma-main-clone/Develop/utils/generateMarkdown.js
@@ -42,9 +42,19 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README based on the license passed in
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === 'Other') {
+    return '';
+  }
+
+  return `
+## License
+This project is covered under the [${license}]( ${renderLicenseLink(license)} ) license.
+`;
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
